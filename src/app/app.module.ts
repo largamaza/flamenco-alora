ef264c9d7241d7492fc63d9e5e989ef627d8191d
ANGEL ANTONIO ROBLEDO SALAZAR <largamaza@gmail.com>
2022-03-13 11:30:14 +0100

    add sidenav with header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,30 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { LoginFormComponent } from './login-form/login-form.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LeftMenuComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+
+    MatSidenavModule,
+    MatListModule
 
   ],
   providers: [],
